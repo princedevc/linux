8a8413d481c264d121fb62f7419138aa29a13342
Olajide Emmanuel <57138729+princedevc@users.noreply.github.com>
2023-04-25 12:22:18 -0500

    update linuc

--- linux.sh
+++ linux.sh
@@ -1,3 +1,18 @@
 while read p; do
 	echo "$p"
 done < readme.txt
+
+########### 
+'''
+use a while loop and accomplish the following:
+
+First variable, PREFIX is the name prefix of a file
+Second variable, TOTAL is the number of files to create
+Using these variables, create the specified number of files, each with the name {PREFIX}{INDEX}
+'''
+INDEX=1
+while [ $INDEX -le $TOTAL ]
+do
+    touch $PREFIX$INDEX
+    ((INDEX++))
+done
